0f2d5ee43c7adeb33a674f52799c08ddda56a431
Mykola Rubashnyi <rubashnyimykola@gmail.com>
2021-01-28 13:19:21 +0200

    second try

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AppHeader from "./components/app-header";
+import TodoList from "./components/todo-list";
+import SearchPanel from "./components/search-panel";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
